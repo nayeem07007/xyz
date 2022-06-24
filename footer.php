86e7ef676c4f12fce70ed1364a0c5ccf9d9304d2
Md Nayeem Farid <mdnayeemfarid@Md-MacBoo-k-Air.local>
2022-06-24 22:08:11 +0600

    footer update

--- footer.php
+++ footer.php
@@ -8,20 +8,6 @@
             
             ?>
 
-<ul class="nav xyz-footer-menu">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
 
 </div>
         </div>
--- header.php
+++ header.php
@@ -18,19 +18,3 @@
             ];
             wp_nav_menu($args);
             
-            ?>
-
-            <ul class="nav">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
