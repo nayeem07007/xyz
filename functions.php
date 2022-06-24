ece4e607469ce57c4908759c73fd80d130b8f852
MD NUR ISLAM <84634226+NURISLAM27878@users.noreply.github.com>
2022-06-24 22:02:48 +0600

    home work 3
    image add
    header menu add
    footer menu add

--- functions.php
+++ functions.php
@@ -2,6 +2,32 @@
 
 function xyz_development_script (){
     wp_enqueue_style('bootstrap' , get_template_directory_uri().'/assets/css/bootstrap.min.css',array(),'1','all' );
+    wp_enqueue_style('xyz-main' , get_template_directory_uri().'/assets/css/main.css',array(),'1','all' );
   
 }
 add_action('wp_enqueue_scripts','xyz_development_script');
+
+if(!function_exists('xyz_setup')) {
+
+    function xyz_setup() {
+        /**
+         * xyz theme setup
+         * @scince 1.0
+         * @return void
+         */
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_image_size('image_400x400', 400, 400, true);
+
+        register_nav_menus([
+            'header_menu' => esc_html__( 'header menu', 'xyz' ),
+            'footer_menu' => esc_html__( 'footer menu', 'xyz' ),
+            'header_menu' => esc_html__( 'header menu', 'xyz' ),
+        ]);
+
+
+
+    }
+
+    add_action('after_setup_theme', 'xyz_setup');
+}
--- index.php
+++ index.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?>
-</head>
-<body>
-        <div class="main">
-            <div class="container">
+<?php
+    get_header();
+?>
                 <?php while(have_posts()):the_post(); ?>
                     <div class="card">
                        <?php if(has_post_thumbnail()){
-                        the_post_thumbnail('full');
+                        the_post_thumbnail('full', ['class'=>'img-fluid']);
                        }?>
                        
                          <div class="card-body">
@@ -24,14 +16,14 @@
                         </div>
                     </div>
                <?php endwhile; ?>
-            </div>
-        </div>
+       
 
 
+<?php
 
+    get_footer();
+
+?>
     
     
-   
-    <?php wp_footer(); ?>
-</body>
-</html>
+  
